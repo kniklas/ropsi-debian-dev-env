3be91cb20fc77cf0ed4ef6e7d6c0c6e93e9196cd
Kamil Niklasinski <kamil@niklasinski.com>
2015-04-04 23:14:09 +0200

    Update git setup with dependencies and install

--- setup-git.sh
+++ setup-git.sh
@@ -1,19 +1,31 @@
+# Install dependencies
+sudo apt-get install asciidoc autoconf libcurl4-gnutls-dev libexpat1-dev gettext libz-dev libssl-dev build-essential
 
+# Install
+cd ~/src/git/
+sudo make prefix=/usr install install-doc install-html install-info
+make configure 
+./configure --prefix=/usr
+make all doc
+sudo make install install-doc install-html
+
+# Setup Manually if required
 # git config --global user.email <email>
 # git config --global user.name <name>
-
-# Set aliases
-# git config --global alias.<alias-name> <git-command>
-
 # git config --system core.editor <editor>
 
-# To show current config
-
-
+# Follow simple rule for push (only current branch)
 git config --global push.default simple
 
+# Set aliases
 git config --global alias.st status
 git config --global alias.co checkout
 git config --global alias.ci commit
 
+# To show current config
 git config --list
+
+git --version
+
+# In order to clone use:
+# git clone https://github.com/git/git.git
