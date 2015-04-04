6e346fc162cbc6ae298240673d6d8f1bf55a90ac
Kamil Niklasinski <kamil@niklasinski.com>
2015-04-04 20:15:49 +0200

    Add aliases: st, co, ci
    Status, Checkout, Commit
    	modified:   setup-git.sh

--- setup-git.sh
+++ setup-git.sh
@@ -8,4 +8,10 @@
 # git config --system core.editor <editor>
 
 # To show current config
+
+
+git config --global alias.st status
+git config --global alias.co checkout
+git config --global alias.ci commit
+
 git config --list
