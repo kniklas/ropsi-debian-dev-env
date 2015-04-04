d40825e98b1ac154fafe9f437902d5a6695c18e2
Kamil Niklasinski <kamil@niklasinski.com>
2015-04-04 23:52:14 +0200

    Add install commands to ratpoison setup

--- setup-ratpoison.sh
+++ setup-ratpoison.sh
@@ -1,4 +1,22 @@
 # Ratpoison setup
 
 # Install dependencies
-sudo apt-get install xcompmgr
+sudo apt-get install \ 
+	xcompmgr \ # required for transparency config
+	gcc make \ # required for compilation
+	xorg xorg-dev # required for X dependencies
+
+# Donlowad source
+cd ~/src
+wget http://download.savannah.nongnu.org/releases/ratpoison/ratpoison-1.4.8.tar.xz
+tar -xf ratpoison-1.4.8.tar.xz
+cd ratpoison-1.4.8
+
+# Install
+./configure
+make
+sudo make install
+
+# Perform additional configuration if required
+* .xinitrc
+* .ratpoisonrc
