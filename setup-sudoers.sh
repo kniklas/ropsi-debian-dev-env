f569845f3793699868ea668d2c6b555257afd5dc
Kamil Niklasinski <kamil@niklasinski.com>
2015-04-05 23:03:16 +0200

    [#2] Add sudoers configuration

---
+++ setup-sudoers.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# add user to sudo group and configure sudoers
+su
+apt-get install sudo
+adduser kamil sudo
+visudo
+
+echo "in editor, below line: 'root ALL=(ALL) ALL'"
+echo "add new line: 'kamil ALL=(ALL) ALL'"
+echo "press CTRL-X and Y."
+
