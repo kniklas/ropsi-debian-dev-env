ffaf33456b2d4b05ab2872ee876c22cb5bf3fe5f
Kamil Niklasinski <kamil@niklasinski.com>
2015-04-06 20:20:16 +0200

    Add Dillo browser
    Browser is added to allow browsing web pages - e.g. Cantera help files build
    in src folder.
    Dependences (FLTK) are handled by the setup file

---
+++ setup-dillo.sh
@@ -0,0 +1,22 @@
+# Install DILLO
+
+# Get dependencies
+cd ~/src
+
+# Install FLTK
+wget http://fltk.org/pub/fltk/1.3.3/fltk-1.3.3-source.tar.gz
+tar -xvf fltk-1.3.3-source.tar.gz
+cd fltk-1.3.3-source.tar.gz
+make
+sudo make install
+
+# Install Dillo
+# if requied remove previous dillo versions: make uninstall
+wget http://www.dillo.org/download/dillo-3.0.4.1.tar.bz2
+tar -xvf dillo-3.0.4.1.tar.bz2
+cd dillo-3.0.4.1
+
+# if required clean previous binaries: make clean
+./configure
+make
+sudo make install
