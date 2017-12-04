96a9e4d48e95b0eb557260511af0f70d2537ed03
Kamil Niklasinski <kamil@niklasinski.com>
2017-12-04 22:58:04 +0100

    Modify midori install script to use apt-get
    It appeardy only `sudo apt-get install midori` is required to install
    midori browser.

--- setup-midori.sh
+++ setup-midori.sh
@@ -2,19 +2,20 @@
 
 # Install MIDORI browser
 # Check http://midori-browser.org for actual version!
+ 
+# Probably only below command is needed, if does not work, then further part
+
+sudo apt-get install midori
 
 # Get the dependencies (needed when 0.5.11 version was installed)
-sudo apt-get install libgcr-base-3-1 hicolor-icon-theme man-db desktop-file-utils mime-support
+# sudo apt-get install libgcr-base-3-1 hicolor-icon-theme man-db desktop-file-utils mime-support
 
 # Set MIDORI download file version
-#MVER=midori_0.5.9-1_amd64_.deb
-MVER=midori_0.5.11-0_amd64_.deb
+#MVER=midori_0.5.11-0_amd64_.deb
 
-echo "Downloading and Installing MIDORI VERSION:" $MVER 
-cd /tmp
+#echo "Downloading and Installing MIDORI VERSION:" $MVER 
+#cd /tmp
 
-wget http://midori-browser.org/downloads/$MVER
-#wget http://midori-browser.org/downloads/midori_0.5.9-1_amd64_.deb
-#wget http://midori-browser.org/downloads/midori_0.5.11-0_amd64_.deb
+#wget http://midori-browser.org/downloads/$MVER
 
-sudo dpkg -i $MVER.deb
+#sudo dpkg -i $MVER
